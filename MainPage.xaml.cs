d537992d9026da91e0e5afb4e21248cea86e47a9
maettu999 <mattia.gisiger@gmail.com>
2022-12-15 15:04:40 +0100

    gitignore fix

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class MainPage : ContentPage
 
     public MainPage()
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 1; i <= 100; i++)
         {
             DB.Products.Add(new Product($"Product {i}", i));
         }
