fd0bf97bb5fd12dcd2f77509f3bfd72e159940fa
maettu999 <mattia.gisiger@gmail.com>
2022-12-30 19:15:53 +0100

    stuff

--- HelpPage.xaml.cs
+++ HelpPage.xaml.cs
@@ -8,5 +8,11 @@ public partial class HelpPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void OnSubmitClick(object sender, EventArgs e)
+    {
+        DisplayAlert("Thanks", "I appreciate your feedback", "OK");
+        checkbox.IsChecked = false;
+    }
 }
 
